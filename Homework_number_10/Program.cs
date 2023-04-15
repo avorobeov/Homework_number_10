fca90bde5032af5f9123a7cdb960872150b95f1c
воробйов александр <avorobeov21@gmail.com>
2023-04-15 16:35:06 +0300

    Добавьте файлы проекта.

---
+++ Homework_number_10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_number_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 5;
+            int maximumValue = 96;
+            int step = 7;
+
+            while (value <= maximumValue)
+            {
+                Console.WriteLine(value);
+                value += step;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
